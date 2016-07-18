0c166faece8ea4b799b976dbb855ad74e81c28c5
Heverson B.Ribeiro <heverson.ribeiro@unine.ch>
2016-07-18 22:13:33 +0200

    just added comments

--- myExample1.lua
+++ myExample1.lua
@@ -1,4 +1,4 @@
-function id_based_ring_distance(self, a, b)
+function id_based_ring_cw_distance(self, a, b)
 
 	     local aux_parameters = self:get_distFunc_extraParams()
 
@@ -28,27 +28,27 @@ function main()
 
 -- setting PSS 
 --parameters: c (view size) , h (healing), s (swappig), fanout, cyclePeriod, peer_selection_policy , me
-	local pss = PSS.new(10, 1, 1, 4, 5, "tail", node)
+	local pss = PSS.new(8, 1, 1, 4, 5, "tail", node)
 	Coordinator.addProtocol("pss1", pss)
 
 -- setting TMAN 
 	local tman_base_protocols={pss}
 --parameters: me, view size, cycle_period, base_procotols, active_b_proto, algoId
-	local tman = TMAN.new(node, 8, 5, tman_base_protocols, "pss1")
+	local tman = TMAN.new(node, 4, 5, tman_base_protocols, "pss1")
 
 	Coordinator.addProtocol("tman1", tman)
 
 --Test: Clockwise-ring distance function
-	tman:set_distance_function(id_based_ring_distance)
-	local m = {9} -- number of bits which is used by the distance function to calculate the distance in the ring
-	tman:set_distFunc_extraParams(m)
-	local rep={}
+	tman:set_distance_function(id_based_ring_cw_distance)
+	local m = {8} -- number of bits which is used by the distance function to calculate the distance in the ring
+	tman:set_distFunc_extraParams(m)  -- set_distFunc_extraParams() is a function sets a table with any extra parameter need for calculating the distance function. this table of parameters can be accessed in the provided distance function with the getter  ' self:get_distFunc_extraParams()'
+	local rep={} -- the node representation is a table where the information that distinguishes one node from another is stored. the node payload must be set with this information which is used in order to calculate the distance between 2 nodes.
 	rep[1] = node:getID()
 	node:setPayload(rep)
 
 --launching protocols
 	Coordinator.showProtocols()
-	Coordinator.launch(node, 660, 0)  --parameters: local node ref, running time in seconds, delay to start each protocol
+	Coordinator.launch(node, 300, 0)  --parameters: local node ref, running time in seconds, delay to start each protocol
 
 end
 
