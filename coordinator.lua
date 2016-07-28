7565febf37d3762b9683ed4b7ffe744097620286
Heverson B.Ribeiro <heverson.ribeiro@unine.ch>
2016-07-28 17:19:08 +0200

    only removed logs

--- coordinator.lua
+++ coordinator.lua
@@ -59,27 +59,13 @@ Coordinator.launch=function(node, running_time, delay)
 	
 end
 
--- Check if still needed
---Coordinator.doActive=function()
---	local algo=nil
---	for k, algo in pairs(Coordinator.algos) do
---		--algo=Coordinator.algos[k]
---		if algo.obj~=nil then
---			log:print("[Coordinator.doActive] - COORDINATOR ACTIVE THREAD at node: "..job.position.." for ALGO Seq: "..k.." ALGO id: "..algo.id.." ALGO OBJ: "..tostring(algo.obj))
---			algo.obj:active_thread()
---		else
---			log:print("[Coordinator.doActive] - ALGO Seq: "..k.." is not instantiated")
---		end
---	end
---end
-
 Coordinator.passive_thread=function(algoId, from, buffer)
 	-- msg1 = {"Coordinator.passive_thread", algoId1, sender, buf} 
 	
-	log:print(" DEBUG [Coordinator.passive] - testing variables at node: "..job.position.." INVOKED") 
-	log:print(" DEBUG [Coordinator.passive] - testing variables at node: "..job.position.." received from: "..tostring(from))
-	log:print(" DEBUG [Coordinator.passive] - testing variables at node: "..job.position.." received algoId: "..tostring(algoId))
-	log:print(" DEBUG [Coordinator.passive] - testing variables at node: "..job.position.." received buffer: "..tostring(buffer))
+	--log:print(" DEBUG [Coordinator.passive] - testing variables at node: "..job.position.." INVOKED") 
+	--log:print(" DEBUG [Coordinator.passive] - testing variables at node: "..job.position.." received from: "..tostring(from))
+	--log:print(" DEBUG [Coordinator.passive] - testing variables at node: "..job.position.." received algoId: "..tostring(algoId))
+	--log:print(" DEBUG [Coordinator.passive] - testing variables at node: "..job.position.." received buffer: "..tostring(buffer))
 	
 	if Coordinator.piggybackMsg then
 		for key, value in pairs(buffer) do
@@ -123,31 +109,22 @@ Coordinator.sender=function()
 	-- This msg table will be like  this: sender_buffer={ {dest={peer1}, msgs={msg1, msg2}}  {dest={peer2}, msgs={msg3}}  ... }
 	-- ex: msg1 = {"Coordinator.passive_thread", algoId1, sender, buf, eventToFire, timeout, dst} 
 	-- ex: peer1={ip = "127.0.0.1", port = 200}
-	-- TODO: remove commented logs later
-		--log:print(" DEBUG - [Coordinator.sender] - at node: "..job.position.." RUNNING.")
-	
+
 		Coordinator.sender_msgs_lock:lock()
 			if #Coordinator.senderBuffer > 0 then
-				--log:print(" DEBUG - [Coordinator.sender] - at node: "..job.position.." # of destinations in the SENDERS_BUFFER: "..#Coordinator.senderBuffer)
 				for k,v in pairs(Coordinator.senderBuffer) do 
 					log:print("destination: "..tostring(v.dest[1].id).." "..tostring(v.dest[1].peer.ip).."/"..tostring(v.dest[1].peer.port)..": has ".. tostring(#v.msgs).." messages ")
 					allEventsToFire = {}
 
 					for g,h in pairs(v.msgs) do 
-						--log:print(" DEBUG - [Coordinator.sender] - destination: "..tostring(v.dest[1].id).." "..tostring(v.dest[1].peer.ip).."/"..tostring(v.dest[1].peer.port)..": has ".. tostring(#v.msgs))
-						--log:print(" DEBUG - [Coordinator.sender] - destination: "..tostring(v.dest[1].id).." messages: "..tostring(h[1]).." algoId: "..tostring(h[2]) )
-
-						--log:print(" DEBUG - [Coordinator.sender] - filling events to fire with: "..h[5] )
 						allEventsToFire[#allEventsToFire+1] = h[5]
 						timeout = h[6]
 						sender = h[3]
 					end
 					events.thread(function() Coordinator.ship(sender, v, timeout, allEventsToFire) end)
 				end 
-				--log:print(" DEBUG - [Coordinator.sender] - all msgs handled, cleaning Coordinator.senderBuffer ")
 				Coordinator.senderBuffer={} 
 			else
-				--log:print(" DEBUG - [Coordinator.sender]  at node: "..job.position.." #Coordinator.senderBuffer not > 0 : Nothing to send")
 			end
 		Coordinator.sender_msgs_lock:unlock()
 
@@ -159,29 +136,21 @@ Coordinator.ship=function(sender, v, timeout, allEventsToFire)
 		--log:print("[Coordinator.ship] - ship THREAD at node: "..job.position.." id: "..sender.id.." sending "..#v.msgs.." protocols")
 
 		local ok = rpc.acall(v.dest[1].peer,{"Coordinator.passive_thread", "msgs",   sender, v.msgs}, timeOut)
-
 		if not ok then
 			Coordinator.totalFailedMsgs=Coordinator.totalFailedMsgs+1
-			--log:print("[Coordinator.send] - COORDINATOR at node: "..job.position.." id: "..sender.id.." exchange with peer "..v.dest[1].id.." not completed, continuing.")
-			--log:print("[Coordinator.send] - COORDINATOR at node: "..job.position.." events to fire: "..#allEventsToFire)
 			for k,ev in pairs(allEventsToFire) do
-				--log:print("[Coordinator.send] - COORDINATOR at node: "..job.position.." event: "..tostring(ev))
 				events.fire(ev)
 			end
 		else
 			Coordinator.totalOkMsgs=Coordinator.totalOkMsgs+1
-			--log:print("[Coordinator.ship] - ship THREAD received OK from RPC.ACALL() at node: "..job.position.." id: "..sender.id.." sending "..#v.msgs.." to "..v.dest[1].id)	
 		end
-		--log:print("[Coordinator.send] - at node: "..job.position.." COORDINATOR at node: "..job.position.." id: "..sender.id.." TOTAL_OK_MSGS: "..Coordinator.totalOkMsgs.." TOTAL_FAILED_MSGS: "..Coordinator.totalFailedMsgs)
+		
 end
 
 Coordinator.send=function(algoId, dst, buf, eventToFire, invokingProtocolID)
 	---- This msg table will be like  this: sender_buffer={ {dest={peer1}, msgs={msg1, msg2}}  {dest={peer2}, msgs={msg3}}  ... }
 	-- ex: msg1 = {"Coordinator.passive_thread", algoId1, sender, buf, eventToFire, timeout, dst} 
 	-- ex: peer1={ip = "127.0.0.1", port = 200}
-	-- TODO: remove commented logs later
-
-		--log:print(" DEBUG - [Coordinator.SEND] - at node: "..job.position.." - COORDINATOR SEND method invoked at node: "..job.position.." by protocol: "..invokingProtocolID)
 
 		local algo = nil
 		for k,v in pairs(Coordinator.algos) do 
@@ -195,18 +164,14 @@ Coordinator.send=function(algoId, dst, buf, eventToFire, invokingProtocolID)
 			local sender = {ip=algo.me.peer.ip, port=algo.me.peer.port, id=algo.me.id}
 
 			if Coordinator.piggybackMsg then
-				--log:print(" DEBUG - [Coordinator.SEND] -  at node: "..job.position.." COORDINATOR SEND - Coordinator.piggybackMsg active")
-				--log:print(" DEBUG - [Coordinator.SEND] -  at node: "..job.position.." preparing new msg ")
 				newMsg = {"Coordinator.passive_thread", algoId, sender, buf, eventToFire, timeOut, dst} 
 
 				Coordinator.sender_msgs_lock:lock()
 
 					if #Coordinator.senderBuffer == 0 then
-						--log:print(" DEBUG - [Coordinator.SEND] -  at node: "..job.position.." COORDINATOR SEND -  #Coordinator.senderBuffer == 0 ")
+
 						Coordinator.senderBuffer[#Coordinator.senderBuffer+1] = { dest={dst}, msgs={newMsg} }
-						--log:print(" DEBUG - [Coordinator.SEND] - at node: "..job.position.." adding newMSG dest.id: "..tostring(dst.id).." newMSG dest.ip: "..tostring(dst.peer.ip).." newMSG dest.port: "..tostring(dst.peer.port))
 					else
-						--log:print(" DEBUG - [Coordinator.SEND] - at node: "..job.position.." COORDINATOR SEND -  #Coordinator.senderBuffer not equal 0 ")
 						found = false
 						for k,v in pairs(Coordinator.senderBuffer) do 
 							if (v.dest[1]==dst) then 
@@ -216,23 +181,20 @@ Coordinator.send=function(algoId, dst, buf, eventToFire, invokingProtocolID)
 						end
 						if not found then
 							Coordinator.senderBuffer[#Coordinator.senderBuffer+1] = { dest={dst}, msgs={newMsg} }
-							--log:print(" DEBUG - [Coordinator.SEND] - at node: "..job.position.." adding newMSG dest.id: "..tostring(dst.id).." newMSG dest.ip: "..tostring(dst.peer.ip).." newMSG dest.port: "..tostring(dst.peer.port))
 						end
 						
 					end
-					--log:print(" DEBUG - [Coordinator.SEND] - at node: "..job.position.." - COORDINATOR SEND - print Coordinator.senderBuffer: ")
+
 					Coordinator.printSenderBuffer()
-				Coordinator.sender_msgs_lock:unlock()  -- note: it seems to be too long this lock(), change to work with a copy to improve performance?
-				
+				Coordinator.sender_msgs_lock:unlock()  
 				
 			else
 				--local timeOut = math.ceil(algo.cycle_period/2)
 				--local sender = {ip=algo.me.peer.ip, port=algo.me.peer.port, id=algo.me.id}
-				--log:print("[Coordinator.send] - at node: "..job.position.." COORDINATOR SEND at node: "..job.position.." id: "..sender.id.." at protocol: "..algoId.." ")
+				log:print("[Coordinator.send] - at node: "..job.position.." COORDINATOR SEND at node: "..job.position.." id: "..sender.id.." at protocol: "..algoId.."  sending msg to "..dst.id)
 				local ok = rpc.acall(dst.peer,{"Coordinator.passive_thread", algoId, sender, buf}, timeOut)
 				if not ok then
 					Coordinator.totalFailedMsgs=Coordinator.totalFailedMsgs+1
-					--log:print("[Coordinator.send] - at node: "..job.position.." COORDINATOR at node: "..job.position.." id: "..sender.id.." exchange with peer "..dst.id.." not completed, continuing.")
 					events.fire(eventToFire)
 				else
 					Coordinator.totalOkMsgs=Coordinator.totalOkMsgs+1
@@ -261,7 +223,8 @@ Coordinator.printSenderBuffer = function()
 end
 
 Coordinator.callAlgoMethod = function(algoId, method, payload, dst, srcId)
--- TODO: possible change in the callback if changes in the send for piggybacked msgs show satisfying results. 
+-- TODO: possible change in the callback if changes in the send for piggybacked msgs show satisfying results.
+
 	log:print("[Coordinator.CALLALGOMETHOD] - COORDINATOR at node: "..job.position.." callAlgoMethod invoked from node: "..srcId.." for method: "..method.." of protocol: "..algoId.." at node: "..dst.id)
 	local ok = rpc.acall(dst, {"Coordinator.dispatch", algoId, method, payload, srcId}, 3)
 	if not ok then 
